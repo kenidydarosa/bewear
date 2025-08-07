9c4e9a1792c33bf3534d31a11fed7781cb5d7e43
kenidydarosa <kenidyy@hotmail.com>
2025-08-06 22:46:59 -0300

    chore: add eslint, prettier and shadcn setup

---
+++ src/app/products/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import React from "react";
+
+const Products = () => {
+  return (
+    <div className="border-radius-md mx-6 flex items-center justify-center bg-emerald-500 px-4 text-red-500">
+      <Button variant="default">Teste</Button>
+    </div>
+  );
+};
+
+export default Products;
