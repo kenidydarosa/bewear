fa87cce9a65b38374a166a03a6113828be3d325b
Kenidy <kenidy.rosa@plasson.com.br>
2025-08-15 13:22:22 -0300

    feat: Create authentication component

---
+++ src/app/authentication/components/sign-up-form.tsx
@@ -0,0 +1,108 @@
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import z from "zod";
+
+const formSchema = z.object({
+    name: z.string("Nome deve ter apenas letras!").min(3, "Nome deve ter no mínimo 3 caracteres"),
+    email: z.email("E-mail inválido!"),
+    password: z.string("Senha inválida!").min(8, "Senha inválida!"),
+  });
+
+type FormValues = z.infer<typeof formSchema>;
+
+const SignUp = () => {
+  const form = useForm<FormValues>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
+    },
+  });
+
+  function onSubmit(values: FormValues) {
+    console.log(values);
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <Card>
+          <CardHeader>
+            <CardTitle>Criar conta</CardTitle>
+            <CardDescription>Crie sua conta para continuar.</CardDescription>
+          </CardHeader>
+          <CardContent className="grid gap-6">
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Nome</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Digite seu nome" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Digite seu email" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Senha</FormLabel>
+                  <FormControl>
+                    <Input
+                      placeholder="Digite a sua senha"
+                      {...field}
+                      type="password"
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </CardContent>
+          <CardFooter>
+            <Button type="submit">Criar conta</Button>
+          </CardFooter>
+        </Card>
+      </form>
+    </Form>
+  );
+};
+
+export default SignUp;
--- src/app/authentication/page.tsx
+++ src/app/authentication/page.tsx
@@ -1,84 +1,26 @@
-import { AppWindowIcon, CodeIcon } from "lucide-react"
+'use client'
 
-import { Button } from "@/components/ui/button"
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
-import {
-  Tabs,
-  TabsContent,
-  TabsList,
-  TabsTrigger,
-} from "@/components/ui/tabs"
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import SignInForm from "./components/sign-in-form";
+import SignUp from "./components/sign-up-form";
 
 export function Authentication() {
   return (
-    <div className="flex w-full max-w-sm flex-col gap-6 p-5 ">
+    <div className="flex w-full max-w-sm flex-col gap-6 p-5">
       <Tabs defaultValue="sign-in">
         <TabsList>
           <TabsTrigger value="sign-in">Entrar</TabsTrigger>
           <TabsTrigger value="sign-up">Criar conta</TabsTrigger>
         </TabsList>
         <TabsContent value="sign-in">
-          <Card>
-            <CardHeader>
-              <CardTitle>Entrar</CardTitle>
-              <CardDescription>
-                Faça login para continuar.
-              </CardDescription>
-            </CardHeader>
-            <CardContent className="grid gap-6">
-              <div className="grid gap-3">
-                <Label htmlFor="tabs-demo-name">Email</Label>
-                <Input id="email" placeholder="Digite seu email" />
-              </div>
-              <div className="grid gap-3">
-                <Label htmlFor="tabs-demo-username">Senha</Label>
-                <Input id="password" placeholder="Digite sua senha" />
-              </div>
-            </CardContent>
-            <CardFooter>
-              <Button>Entrar</Button>
-            </CardFooter>
-          </Card>
+          <SignInForm />
         </TabsContent>
         <TabsContent value="sign-up">
-          <Card>
-            <CardHeader>
-              <CardTitle>Criar conta</CardTitle>
-              <CardDescription>
-                Crie sua conta para continuar.
-              </CardDescription>
-            </CardHeader>
-            <CardContent className="grid gap-6">
-              <div className="grid gap-3">
-                <Label htmlFor="tabs-demo-current">Nome</Label>
-                <Input id="name" placeholder="Digite seu nome" />
-              </div>
-              <div className="grid gap-3">
-                <Label htmlFor="tabs-demo-new">Email</Label>
-                <Input id="email"  placeholder="Digite seu email" />
-              </div>
-              <div className="grid gap-3">
-                <Label htmlFor="tabs-demo-new">Senha</Label>
-                <Input id="password"  placeholder="Digite sua senha"/>
-              </div>
-            </CardContent>
-            <CardFooter>
-              <Button>Criar conta</Button>
-            </CardFooter>
-          </Card>
+          <SignUp />
         </TabsContent>
       </Tabs>
     </div>
-  )
+  );
 }
 
-export default Authentication
+export default Authentication;
